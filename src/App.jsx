bc1a137013bf6305e20945f11efbd9a11d96c704
vaibhavgogo <vaibhavsmartkid@gmail.com>
2025-09-14 13:11:34 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Test from "@/components/Test"
+
+export default function App() {
+  return (
+    <div className="p-6">
+      <Test />
+    </div>
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-
+import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    open:true,
+    port :3000,
+  }, resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
